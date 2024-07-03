7ba21bd612b25152406533407c693f5b7c080435
Max Rohde <maxdrohde@gmail.com>
2024-07-02 23:04:28 -0500

    new

--- R/fit_rand_eff.R
+++ R/fit_rand_eff.R
@@ -4,14 +4,16 @@ fit_rand_eff <- function(df,
   df$y <- as.ordered(df$y)
 
   mod_full <- ordinal::clmm(formula = y ~ splines::ns(t, df = knots+ 1) * tx + (1|id),
-                            link = "logit",
+                            link = "logit", control = ordinal::clmm.control(maxIter = 1000),
                             data = df)
 
   mod_reduced <- ordinal::clmm(formula = y ~ splines::ns(t, df = knots+ 1) + (1|id),
-                               link = "logit",
+                               link = "logit", control = ordinal::clmm.control(maxIter = 1000),
                                data = df)
 
-  tx_p_value <- as.data.frame(anova(mod_full, mod_reduced))$`Pr(>Chisq)`[[2]]
+  res <- ordinal:::anova.clm(mod_full, mod_reduced)
+
+  tx_p_value <- as.data.frame(res)$`Pr(>Chisq)`[[2]]
 
   return(tx_p_value)
 }
