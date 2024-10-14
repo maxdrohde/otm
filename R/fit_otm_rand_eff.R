fdb0292f18e80ba1ab33ff55fc1a5b0054be1f3a
Max Rohde <maxdrohde@gmail.com>
2024-10-14 16:39:11 -0500

    new

---
+++ R/fit_otm_rand_eff.R
@@ -0,0 +1,45 @@
+fit_otm_rand_eff <- function(df, knots, rand_slope){
+
+  if (rand_slope) {
+    mod_full <-
+      ordinal::clmm(formula = as.ordered(y) ~ yprev + splines::ns(t, df = knots+ 1) * tx + (1 + t|id),
+                    nAGQ=1,
+                    link = "logit",
+                    data = df)
+
+    mod_reduced <-
+      ordinal::clmm(formula = as.ordered(y) ~ yprev + splines::ns(t, df = knots+ 1) + (1 + t|id),
+                    nAGQ=1,
+                    link = "logit",
+                    data = df)
+  } else{
+    mod_full <-
+      ordinal::clmm(formula = as.ordered(y) ~ yprev + splines::ns(t, df = knots+ 1) * tx + (1|id),
+                    nAGQ=10,
+                    link = "logit",
+                    data = df)
+
+    mod_reduced <-
+      ordinal::clmm(formula = as.ordered(y) ~ yprev + splines::ns(t, df = knots+ 1) + (1|id),
+                    nAGQ=10,
+                    link = "logit",
+                    data = df)
+  }
+
+  a <- anova(mod_full, mod_reduced) |> as.data.frame()
+
+  tx_p_value <- a$`Pr(>Chisq)`[[2]]
+
+  return(tx_p_value)
+}
+
+safe_fit_otm_rand_eff <- function(df, knots, rand_slope) {
+  tryCatch({
+    fit_otm_rand_eff(df, knots, rand_slope)
+  },
+  error = function(e) {
+    print("There was an error in the OTM random intecept model")
+    print(e)
+    return(NA)
+  })
+}
